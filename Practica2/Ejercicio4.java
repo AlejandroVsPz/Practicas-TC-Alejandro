937e8147fe17c476a6d96558ac6b5b26f481f770
alex_vargasp <alex.vargas.p@outlook.com>
2024-04-05 02:00:01 -0600

    Comentarios

--- Practica2/Ejercicio4.java
+++ Practica2/Ejercicio4.java
@@ -4,13 +4,13 @@ package Practica2;
 
 public class Ejercicio4 {
     public static void main(String[] args) {
-        int base = 5;
-        int expo = 5;
-        int res = 1;
-        for(int i = 0; i< expo; i++){
-            res = res * base;
+        int base = 5; // el numero
+        int expo = 5; // el exponente
+        int res = 1; // variable resultado de la multiplicacion
+        for (int i = 0; i < expo; i++) { // ciclo para hacer las multiplicaciones
+            res = res * base; 
         }
-        System.out.println(res);
+        System.out.println(res); // imprime el resultado
     }
 
 }
--- Practica2/Ejercicio5.java
+++ Practica2/Ejercicio5.java
@@ -5,15 +5,15 @@ que calcule si un número es primo. */
 
 public class Ejercicio5 {
     public static void main(String[] args) {
-        int num = 17;
-        int suma = 0;
-        for(int i = 2; i<num; i++){
+        int num = 17; // Variable del numero a calcular si es primo o no
+        int suma = 0; // incrementa si el mod % es 0
+        for(int i = 2; i<num; i++){ // ciclo para hacer las divisiones
             if (num % i == 0) {
                 suma = suma + 1;
             }
         }
 
-        if (suma > 0) {
+        if (suma > 0) { // condicion si es mayor a 0 no es primo
             System.out.println(num + " No es primo");
         }else if (suma == 0) {
             System.out.println(num + " Si es primo");
